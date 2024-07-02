68158940f60c7e4e2667a85192454b7fe8e238fc
Caetano Santana <santanacaetano.m@gmail.com>
2024-07-02 16:44:00 -0400

    feat: cria pastas para páginas Root e páginas de Auth

--- thermo-app/app/(root)/page.tsx
+++ thermo-app/app/(root)/page.tsx
@@ -1,8 +1,22 @@
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
 function Home() {
+  const loggedIn = {firstName: 'Miguel'}
+
   return (
-    <div>home page</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type='greeting'
+            title='Bem vindo ao Thermo'
+            user={loggedIn?.firstName || 'Convidado'}
+            subtext='Acompanhe e monitore em tempo real a curagem do concreto de suas obras'
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
